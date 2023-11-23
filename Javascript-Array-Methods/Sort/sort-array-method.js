87812744c909503e370df5b27534cb40ecd4d5bc
Johan Swan <jswannieman@gmail.com>
2023-11-24 10:09:07 +1300

    Update sort-array-method JS Component

--- Javascript-Array-Methods/Sort/sort-array-method.js
+++ Javascript-Array-Methods/Sort/sort-array-method.js
@@ -29,9 +29,9 @@ const persons = [
 
 const sort2 = persons.sort((x, y) => x.age - y.age);
 
-if (sort2 instanceof Array) {
-  alert("Yes it is an array");
-}
+// if (sort2 instanceof Array) {
+//   alert("Yes it is an array");
+// }
 
 const sorter2 = document.getElementById("sort2");
 
@@ -41,5 +41,19 @@ for (const value in sort2) {
   if (sort2.hasOwnProperty(value)) {
     const element = sort2[value];
     sorter2.innerHTML += `Name : ${element.name} is ${element.age} old | `;
+  }
+}
+
+// OR
+
+const sorter3 = document.getElementById("sort3");
+
+sorter3.innerHTML = `List : `;
+
+for (let item of sort2) {
+  for (let value in item) {
+    if (item.hasOwnProperty(value)) {
+      sorter3.innerHTML += `${value} is ${item[value]} | `;
+    }
   }
 }

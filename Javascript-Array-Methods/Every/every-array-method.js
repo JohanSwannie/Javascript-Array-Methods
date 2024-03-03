8faae0d48a332cf71684acb8d66c30e34001a8b0
Johan Swan <jswannieman@gmail.com>
2024-03-03 19:05:57 +1300

    Update every-array-method JS Component

---
+++ Javascript-Array-Methods/Every/every-array-method.js
@@ -0,0 +1,15 @@
+const every1 = document.getElementById("every1");
+
+const list1 = [12, "sport", true, 18, 19.35, false];
+
+const result1 = list1.every((item) => item / 3 === 6);
+
+every1.innerHTML = `list1 = ${result1}`;
+
+const every2 = document.getElementById("every2");
+
+const isBiggerThan10 = (element) => element > 10;
+
+every2.innerHTML = `Is every element in the array bigger than 10 - ${[
+  12, 15, 18, 11, 14,
+].every(isBiggerThan10)}`;

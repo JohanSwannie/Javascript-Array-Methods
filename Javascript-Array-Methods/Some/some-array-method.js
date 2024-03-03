369cd04a7356d61d1d61aee938a26af600f283b0
Johan Swan <jswannieman@gmail.com>
2024-03-03 18:49:45 +1300

    Update some-array-method JS Component

---
+++ Javascript-Array-Methods/Some/some-array-method.js
@@ -0,0 +1,17 @@
+const some1 = document.getElementById("some1");
+
+const list1 = [12, "sport", true, 18, 19.35, false];
+
+const result1 = list1.some((item) => item / 3 === 6);
+
+some1.innerHTML = `list1 = ${result1}`;
+
+const some2 = document.getElementById("some2");
+
+function isBiggerThan10(element) {
+  return element > 10;
+}
+
+some2.innerHTML = `Are any element in the array bigger than 10 - ${[
+  2, 5, 8, 11, 4,
+].some(isBiggerThan10)}`;
